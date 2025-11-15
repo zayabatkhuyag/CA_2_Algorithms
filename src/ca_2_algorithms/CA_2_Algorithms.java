bc50775e58b54ac0ba74fcfdb892c2a0b4ff3521
zayabatkhuyag <zayab2112@gmail.com>
2025-11-15 00:20:04 +0000

    Started working on the assignment

---
+++ src/ca_2_algorithms/CA_2_Algorithms.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca_2_algorithms;
+
+/**
+ *
+ * @author zayab
+ */
+public class CA_2_Algorithms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
